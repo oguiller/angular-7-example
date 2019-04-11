68adc3cbae412a9246b34ac94264e6a7730045ef
klm75982 <guillermo.rodriguezfernandez@klm.com>
2019-04-11 10:06:37 +0200

    Setting Classes Using the ngClass Directive

--- src/app/component.ts
+++ src/app/component.ts
@@ -13,4 +13,15 @@ export class ProductComponent {
     return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
   }
 
+  // The getClassMap method returns an object with properties whose values are one or more class names,
+  // with values based on the property values of the Product object whose key is specified as the method argument.
+
+  getClassMap(key: number): Object {
+    let product = this.model.getProduct(key);
+    return {
+      "text-center bg-danger": product.name == "Kayak",
+      "bg-info": product.price < 50
+    };
+  }
+
 }
