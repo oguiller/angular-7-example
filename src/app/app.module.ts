b0c64496b1309989b2244ca8245bbb75ff8379d4
klm75982 <guillermo.rodriguezfernandez@klm.com>
2019-04-26 13:39:09 +0200

    Preparing app for next steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 // import { AppComponent } from './app.component';
 import { ProductComponent } from './component';
 
@@ -10,7 +10,7 @@ import { ProductComponent } from './component';
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [ProductComponent]
--- src/app/component.ts
+++ src/app/component.ts
@@ -1,17 +1,7 @@
-import { ApplicationRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { Product } from './product.model';
 import { Model } from './repository.model';
 
-
-/*
- We can try this in the browser console:
-   model.products.shift() -- This statement calls the shift method on the array of Product object.
-
-   appRef.tick() -- The tick method starts the Angular change detection process, where Angular looks at the data in the
- application and the expressions in the data binding and processes any changes. The data bindings in the template use
- specific array indexes to display data, and now that an object has been removed from the model, the bindings will be
- updated to display new values
- */
 @Component({
   selector: 'app',
   templateUrl: 'template.html'
@@ -19,39 +9,11 @@ import { Model } from './repository.model';
 export class ProductComponent {
   model: Model = new Model();
 
-  constructor(ref: ApplicationRef) {
-    (<any>window).appRef = ref;
-    (<any>window).model = this.model;
-    /*
-    These statements define variables in the global namespace and assign the ApplicationRef and Model objects to them.
-    It is good practice to keep the global namespace as clear as possible, but exposing these objects allows them
-    to be manipulated through the browser’s JavaScript console, which is important for this example.
-     */
-  }
-
-  getProductByPosition(position: number): Product {
-    return this.model.getProducts()[position];
-  }
-
-  getClassesByPosition(position: number): string {
-    let product = this.getProductByPosition(position);
-    return 'p-2 ' + (product.price < 50 ? 'bg-info' : 'bg-warning');
-  }
-
   getProduct(key: number): Product {
     return this.model.getProduct(key);
   }
 
   getProducts(): Product[] {
     return this.model.getProducts();
-  }
-
-  getProductCount(): number {
-    console.log('getProductCount invoked');
-    return this.getProducts().length;
-  }
-
-  getProductPrice(index: number): number {
-    return Math.floor(this.getProduct(index).price);
   }
 }
