3c548b1a8c9f70021f55ff6294a390abdb934d92
klm75982 <guillermo.rodriguezfernandez@klm.com>
2019-05-06 22:22:30 +0200

    Creating custom events

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,19 +1,34 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Input, Output, SimpleChange } from '@angular/core';
+import { Product } from './product.model';
 
 @Directive({
   selector: '[pa-attr]'
 })
 export class PaAttrDirective {
+
   constructor(private element: ElementRef) {
-    console.log('Constructor');
+    this.element.nativeElement.addEventListener('click', e => {
+      if (this.product != null) {
+        this.click.emit(this.product.category);
+      }
+    });
   }
 
   @Input('pa-attr')
   bgClass: string;
+  @Input('pa-product')
+  product: Product;
+  @Output('pa-category')
+  click = new EventEmitter<string>();
 
-  ngOnInit() {
-    console.log('On init');
-    this.element.nativeElement.classList.add(this.bgClass || 'bg-success',
-      'text-white');
+  ngOnChanges(changes: { [property: string]: SimpleChange }) {
+    let change = changes['bgClass'];
+    let classList = this.element.nativeElement.classList;
+    if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+      classList.remove(change.previousValue);
+    }
+    if (!classList.contains(change.currentValue)) {
+      classList.add(change.currentValue);
+    }
   }
 }
