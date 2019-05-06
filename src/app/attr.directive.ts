2b0061dcfe39d7138a57b024a644b133d2b12500
klm75982 <guillermo.rodriguezfernandez@klm.com>
2019-05-06 20:47:05 +0200

    Creating data-bound input properties. - Using ngOnInit

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,10 +1,19 @@
-import { Attribute, Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[pa-attr]'
 })
 export class PaAttrDirective {
-  constructor(element: ElementRef, @Attribute('pa-attr') bgClass: string) {
-    element.nativeElement.classList.add(bgClass || 'bg-success', 'text-white');
+  constructor(private element: ElementRef) {
+    console.log('Constructor');
+  }
+
+  @Input('pa-attr')
+  bgClass: string;
+
+  ngOnInit() {
+    console.log('On init');
+    this.element.nativeElement.classList.add(this.bgClass || 'bg-success',
+      'text-white');
   }
 }
